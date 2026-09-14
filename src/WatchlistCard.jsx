f3fc51930dde3fdb1b344fb24b7fb43368a0fae1
Davecouncil <councildave2@gmail.com>
2026-09-14 16:11:13 +0100

    initial commit

---
+++ src/WatchlistCard.jsx
@@ -0,0 +1,20 @@
+
+export default function WatchlistCard(props) {
+
+  return (
+    <div className="Watchlist">
+      <div>
+        <img src={props.icon} alt={props.base} />
+        <div>
+          <div><em>{props.base}</em>/{props.quote}</div>
+          <p>Live Market</p>
+        </div>
+      </div>
+      <div>
+        <span>{props.price}</span>
+        <span>{props.change}</span>
+        <span>{props.sparkline}</span>
+      </div>
+    </div>
+  )
+}
